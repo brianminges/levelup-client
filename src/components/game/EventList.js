183e867daba4fe89150abbeae2c4b615a7141f3a
brianminges <btminges@gmail.com>
2022-05-26 20:06:51 -0500

    chapter 7 done

---
+++ src/components/game/EventList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react"
+import { getEvents } from "./EventManager.js"
+import "./Event.css"
+
+export const EventList = (props) => {
+    const [ events, setEvents ] = useState([])
+
+    useEffect(() => {
+        getEvents().then(data => setEvents(data))
+    }, [])
+
+
+    return (
+        <article className="events">
+            <h2> Upcoming events</h2>
+            {
+                events.map(event => {
+                    return <section key={`event--${event.id}`} className="event">
+                        <div className="event__title"><strong>{event.game.title}</strong></div>
+                        <div className="event__description"><em>{event.description}</em></div>
+
+
+                        <div className="event__host"><strong>Hosted by:</strong> {event.organizer.user.username}</div>
+                        <div className="event__players"><strong>Date:</strong> {event.date} </div>
+                        <div className="event__skillLevel"><strong>Time:</strong> {event.time}</div>
+                    </section>
+                })
+            }
+        </article>
+    )
+}
---
+++ src/components/game/GameList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react"
+import { getGames } from "./GameManager.js"
+import "./Game.css"
+
+export const GameList = (props) => {
+    const [ games, setGames ] = useState([])
+
+    useEffect(() => {
+        getGames().then(data => setGames(data))
+    }, [])
+
+    return (
+        <article className="games">
+            <h2>List of games</h2>
+            {
+                games.map(game => {
+                    return <section key={`game--${game.id}`} className="game">
+                        <div className="game__title">{game.title} by {game.maker}</div>
+                        <div className="game__players">{game.number_of_players} players needed</div>
+                        <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                    </section>
+                })
+            }
+        </article>
+    )
+}
